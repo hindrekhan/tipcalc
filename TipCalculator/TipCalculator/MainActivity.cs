db013d86ffa3f33007a5959c8348f62e9bc4463b
hindrekhan <33310195+hindrekhan@users.noreply.github.com>
2018-09-13 08:43:16 +0300

    asdf

---
+++ TipCalculator/TipCalculator/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+
+namespace TipCalculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        TextView textView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            textView = FindViewById<TextView>(Resource.Id.textView1);
+            var seekBar = FindViewById<SeekBar>(Resource.Id.seekBar1);
+
+            seekBar.ProgressChanged += SeekBar_ProgressChanged;
+        }
+
+        private void SeekBar_ProgressChanged(object sender, SeekBar.ProgressChangedEventArgs e)
+        {
+            textView.Text = e.Progress.ToString();
+        }
+    }
+}
